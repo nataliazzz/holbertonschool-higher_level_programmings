a2e8cc3fa9dc8c479e15153397c7b02868f3c4c9
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2022-07-17 14:19:06 -0500

    Create 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+error codes
+"""
+from sys import argv
+import urllib.request
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
