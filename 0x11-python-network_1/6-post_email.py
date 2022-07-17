c27e1c913a34b7d63aa93e2a522b658b42abeda8
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2022-07-17 14:21:53 -0500

    Create 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+show response
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    payload = {'email': argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)
