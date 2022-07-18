00965cf8894ea0b01bd699e3a293041e0f42e61b
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2022-07-18 12:56:00 -0500

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,64 +1,71 @@
 #!/usr/bin/python3
 """
-Module contains class Square
+...
 """
-
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """
-    defines class Square; inherits from class Rectangle
+    ...
     """
+
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize"""
+        """
+        ...
+        """
         super().__init__(size, size, x, y, id)
-        self.size = size
+
+    def __str__(self):
+        """
+        ...
+        """
+        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
+            self.id, self.x, self.y, self.width
+        )
 
     @property
     def size(self):
-        """Getter size"""
+        """
+        ...
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """Setter size - sets width and height as size"""
+        """
+        ...
+        """
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """Prints [Square] (<id>) <x>/<y> - <size>"""
-        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
-            self.__class__.__name__, self.id, self.x, self.y,
-            self.size)
-
     def update(self, *args, **kwargs):
-        if args:
-            for k, v in enumerate(args):
-                if k == 0:
-                    self.id = v
-                elif k == 1:
-                    self.size = v
-                elif k == 2:
-                    self.x = v
-                else:
-                    self.y = v
-        else:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "size" in kwargs:
-                self.size = kwargs["size"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            if "y" in kwargs:
-                self.y = kwargs["y"]
+        """
+        ...
+        """
+        argc = len(args)
+        kwargc = len(kwargs)
+        modif_attrs = ['id', 'size', 'x', 'y']
+
+        if argc > 4:
+            argc = 4
+
+        if argc > 0:
+            for i in range(argc):
+                setattr(self, modif_attrs[i], args[i])
+        elif kwargc > 0:
+            for k, v in kwargs.items():
+                if k in modif_attrs:
+                    setattr(self, k, v)
 
     def to_dictionary(self):
-        """Return dictionary representation"""
-        d = {}
-        d["id"] = self.id
-        d["size"] = self.size
-        d["x"] = self.x
-        d["y"] = self.y
-        return d
+        """
+        ...
+        """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
